09200605d2a72c89a167d69d760e2269f1f52304
Eric Moritz <emoritz@gannett.com>
2012-04-20 09:34:55 -0400

    Windows apparently does not like commas in filenames.

--- transmogrify/processors.py
+++ transmogrify/processors.py
@@ -91,11 +91,11 @@ class Crop(Processor):
     @staticmethod
     def param_pattern():
         return re.compile((r"^((\d+)|(x\d+)|(\d+x\d+))"
-                           r"|((\d+),(\d+),(\d+),(\d+))$"))
+                           r"|((\d+)_(\d+)_(\d+)_(\d+))$"))
     
     @staticmethod
     def process(image, size_or_bbox, *args, **kwargs):
-        if "," in size_or_bbox:
+        if "_" in size_or_bbox:
             return Crop.crop_bbox(image, size_or_bbox, *args, **kwargs)
         else:
             return Crop.crop_center(image, size_or_bbox, *args, **kwargs)
@@ -112,7 +112,7 @@ class Crop(Processor):
 
     @staticmethod
     def crop_bbox(image, bbox, *args, **kwargs):
-        bbox = map(int, bbox.split(","))
+        bbox = map(int, bbox.split("_"))
         return image.crop(bbox)
 
 class ForceFit(Processor):
