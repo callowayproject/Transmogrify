989ee12024601c25defd188869c741239f64478c
Corey Oordt <coreyoordt@gmail.com>
2016-04-05 07:37:28 -0500

    Updating the optimization code for PEP8

--- transmogrify/optimize.py
+++ transmogrify/optimize.py
@@ -3,29 +3,29 @@ try:
     from PIL import Image
     from PIL.ImageCms import profileToProfile
 except ImportError:
-    profileToProfile = lambda im, *args, **kwargs: im
+    profileToProfile = lambda im, *args, **kwargs: im  # NOQA
 
 
-def convert_to_RGB(img):
+def convert_to_rgb(img):
     """
     Convert an image to RGB if it isn't already RGB or grayscale
     """
-    inputProfile = "/Users/coordt/Downloads/Adobe ICC Profiles/CMYK Profiles/USWebUncoated.icc"
-    outputProfile = "/Users/coordt/Downloads/sRGB_v4_ICC_preference.icc"
     if img.mode not in ('RGB', 'L'):
-        return profileToProfile(img, inputProfile, outputProfile, outputMode='RGB')
+        profile_dir = os.path.join(os.path.dirname(__file__), 'profiles')
+        input_profile = os.path.join(profile_dir, "USWebUncoated.icc")
+        output_profile = os.path.join(profile_dir, "sRGB_v4_ICC_preference.icc")
+        return profileToProfile(img, input_profile, output_profile, outputMode='RGB')
     return img
 
 
 def get_output_filename(input_filename):
     parent_dir, filename = os.path.split(input_filename)
     base_filename, ext = os.path.splitext(filename)
-    return os.path.join("/Users/coordt/Desktop", "%s.optim%s" % (base_filename, ext))
+    return os.path.join(parent_dir, "%s.optim%s" % (base_filename, ext))
 
 
-def main():
-    input_filename = "/Users/coordt/Desktop/pumpkin_632716265.jpg"
+def test_rgb_conversion(input_filename):
     output_filename = get_output_filename(input_filename)
     original = Image.open(input_filename)
-    modified = convert_to_RGB(original)
+    modified = convert_to_rgb(original)
     modified.save(output_filename, optimized=True, progressive=True)
