12d286a0c96ff7244e876b7ca18c7badfd36416f
Corey Oordt <coreyoordt@gmail.com>
2017-10-06 07:41:29 -0500

    [2.0.6] Update test_serve.sh to copy files from the correct testdata place

--- test_serve.sh
+++ test_serve.sh
@@ -3,9 +3,9 @@
 if [ ! -e "originals" ]
 then
     mkdir originals
-    cp transmogrify/testdata/horiz_img.jpg originals/
-    cp transmogrify/testdata/square_img.jpg originals/
-    cp transmogrify/testdata/vert_img.jpg originals/
+    cp transmogrify/tests/testdata/horiz_img.jpg originals/
+    cp transmogrify/tests/testdata/square_img.jpg originals/
+    cp transmogrify/tests/testdata/vert_img.jpg originals/
 fi
 if [ ! -e "modified" ]
 then
